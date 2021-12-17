3bd4f75c7d260545104e8015ce7c0c0e2f47620d
Ioannis Tziligkakis <gtsiligkakis@gmail.com>
2021-12-17 13:44:33 +0200

    Temp disable of guild settings deletion

--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -1,5 +1,8 @@
 const { DeleteGuildSettings } = require("../lib/resourceRepo");
 
 module.exports = () => async (guild) => {
+  // @TODO: do nothing for now; revert it in the future
+  return;
+
   await DeleteGuildSettings(guild.id);
 };
